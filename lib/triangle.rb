d484fbd720c04927802f3bc95d9f60d91aac022e
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-10-17 10:51:51 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,7 +26,7 @@ class Triangle
       :equilateral
     else if side1 != side2 && side2 != side3 && side1 != side3
       :scalene
-    else if side1 == side2 && side2 == side3 && side1 != side3 || side1 !== side2 && side1 == 3 && side 2 == side3
+    else if side1 == side2 && side2 == side3 && side1 != side3 || side1 !== side2 && side1 == side3 && side 2 == side3
       :isosceles
     end 
   end 
